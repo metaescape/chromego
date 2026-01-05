74393e051d2eadd2db6e248f8b19309fd2476eb7
mE <metaescape@foxmail.com>
2026-01-05 23:37:10 +0800

    fix multiple search bug

--- content.js
+++ content.js
@@ -30,24 +30,35 @@ function autoCloseAlert(message, duration) {
   }, duration);
 }
 
+let selectedText = "";
+function searchSelectedText() {
+  window.open(
+    `https://www.google.com/search?q=如何理解 ”${encodeURIComponent(
+      selectedText
+    )}”`,
+    "_blank"
+  );
+}
+
 document.addEventListener("mouseup", (event) => {
-  const selectedText = window.getSelection().toString().trim();
-  const tooltipId = "selection-tooltip";
-  let tooltip = document.getElementById(tooltipId);
+  setTimeout(() => {
+    selectedText = window.getSelection().toString().trim();
+    const tooltipId = "selection-tooltip";
+    let tooltip = document.getElementById(tooltipId);
 
-  // 如果没有选中任何文本或选中的是空格，则隐藏 tooltip
-  if (!selectedText) {
-    if (tooltip) {
-      tooltip.style.display = "none";
+    // 如果没有选中任何文本或选中的是空格，则隐藏 tooltip
+    if (!selectedText) {
+      if (tooltip) {
+        tooltip.style.display = "none";
+      }
+      return;
     }
-    return;
-  }
 
-  // 如果 tooltip 不存在，则创建它
-  if (!tooltip) {
-    tooltip = document.createElement("div");
-    tooltip.id = tooltipId;
-    tooltip.style.cssText = `
+    // 如果 tooltip 不存在，则创建它
+    if (!tooltip) {
+      tooltip = document.createElement("div");
+      tooltip.id = tooltipId;
+      tooltip.style.cssText = `
       position: absolute;
       background-color: #333;
       color: white;
@@ -61,35 +72,26 @@ document.addEventListener("mouseup", (event) => {
       display: flex;
       gap: 10px;
     `;
-    document.body.appendChild(tooltip);
-  }
-
-  // 更新 tooltip 的内容和位置
-  tooltip.innerHTML = `
+      tooltip.innerHTML = `
     <span class="tooltip-option" data-action="google">搜索</span>
   `;
-  tooltip.style.display = "flex";
-  tooltip.style.left = `${event.pageX}px`;
-  tooltip.style.top = `${event.pageY + 15}px`;
+      document.body.appendChild(tooltip);
+      // 监听 tooltip 内部的点击事件
+      tooltip.addEventListener("click", (e) => {
+        const action = e.target.dataset.action;
+        if (action === "google") {
+          searchSelectedText();
+        }
+        // 点击后隐藏 tooltip
+        tooltip.style.display = "none";
+      });
+    }
 
-  // 监听 tooltip 内部的点击事件
-  tooltip.addEventListener(
-    "click",
-    (e) => {
-      const action = e.target.dataset.action;
-      if (action === "google") {
-        window.open(
-          `https://www.google.com/search?q=如何理解： ${encodeURIComponent(
-            selectedText
-          )}`,
-          "_blank"
-        );
-      }
-      // 点击后隐藏 tooltip
-      tooltip.style.display = "none";
-    },
-    { once: true }
-  ); // 使用 once: true 确保事件监听器只执行一次
+    // 更新 tooltip 位置
+    tooltip.style.display = "flex";
+    tooltip.style.left = `${event.pageX}px`;
+    tooltip.style.top = `${event.pageY + 15}px`;
+  }, 0);
 });
 
 function hideTooltip(event) {
